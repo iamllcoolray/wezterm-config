fd574e97892aec862e57204a5eb9567f1cc204a8
iamllcoolray <llcoolray24@gmail.com>
2025-10-03 21:09:16 -0400

    updated to ensure that the termial uses the correct powershell and zsh

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,19 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+-- Cross-platform default shell
+if wezterm.target_triple:match("windows") then
+    config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-NoLogo" }
+else
+    config.default_prog = { "/bin/zsh" } 
+end
+
+-- Appearance
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 config.window_background_opacity = 0.9
+
+-- Optional
+config.font_size = 11.0
+config.window_decorations= "RESIZE"
 
 return config
